9c4c549b75b541eb35bba5b6103be545cfe25bf0
George Benjamin <msdcconnect@gmail.com>
2021-09-27 13:48:50 +0100

    docs: improve api docs

--- src/interfaces/http/routes/v1/moviesRoute.js
+++ src/interfaces/http/routes/v1/moviesRoute.js
@@ -91,6 +91,10 @@ router
  * @apiDescription Posts a comment for a movie anonymously👻
  * @apiVersion 1.0.0
  * @apiParam {String} content - User's comment (500 characters max)
+ *  @apiParamExample {json} Request-Example:
+ *     {
+ *       "content": "Best film evurrr",
+ *     }
  * @apiSuccessExample Success Response:
  * {
  *     "success": true,
@@ -165,6 +169,12 @@ router
  * @apiParam {String="name", "gender", "height"} sortBy - Field to sort by
  * @apiParam {String="asc", "desc"} order - Sorting order
  * @apiParam {String="male", "female"} gender - Filter by gender
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "sortBy": "gender",
+ *       "order": "asc"
+ *       "gender": "female"
+ *     }
  * @apiVersion 1.0.0
  * @apiSuccessExample Success Response:
  * {
@@ -172,6 +182,68 @@ router
  *     "status_code": 200,
  *     "message": "Successfully posted comment",
  *     "data": {
+ *        "characters": [
+ *          {
+ *              "name": "Leia Organa",
+ *              "height": "150",
+ *              "mass": "49",
+ *              "hair_color": "brown",
+ *              "skin_color": "light",
+ *              "eye_color": "brown",
+ *              "birth_year": "19BBY",
+ *              "gender": "female",
+ *              "homeworld": "https://swapi.dev/api/planets/2/",
+ *              "films": [
+ *                  "https://swapi.dev/api/films/1/",
+ *                  "https://swapi.dev/api/films/2/",
+ *                  "https://swapi.dev/api/films/3/",
+ *                  "https://swapi.dev/api/films/6/"
+ *              ],
+ *              "species": [],
+ *              "vehicles": [
+ *                  "https://swapi.dev/api/vehicles/30/"
+ *              ],
+ *              "starships": [],
+ *              "created": "2014-12-10T15:20:09.791000Z",
+ *              "edited": "2014-12-20T21:17:50.315000Z",
+ *              "url": "https://swapi.dev/api/people/5/"
+ *          },
+ *          {
+ *              "name": "Luke Skywalker",
+ *              "height": "172",
+ *              "mass": "77",
+ *              "hair_color": "blond",
+ *              "skin_color": "fair",
+ *              "eye_color": "blue",
+ *              "birth_year": "19BBY",
+ *              "gender": "male",
+ *              "homeworld": "https://swapi.dev/api/planets/1/",
+ *              "films": [
+ *                  "https://swapi.dev/api/films/1/",
+ *                  "https://swapi.dev/api/films/2/",
+ *                  "https://swapi.dev/api/films/3/",
+ *                  "https://swapi.dev/api/films/6/"
+ *              ],
+ *              "species": [],
+ *              "vehicles": [
+ *                  "https://swapi.dev/api/vehicles/14/",
+ *                  "https://swapi.dev/api/vehicles/30/"
+ *              ],
+ *              "starships": [
+ *                  "https://swapi.dev/api/starships/12/",
+ *                  "https://swapi.dev/api/starships/22/"
+ *              ],
+ *              "created": "2014-12-09T13:50:51.644000Z",
+ *              "edited": "2014-12-20T21:17:56.891000Z",
+ *              "url": "https://swapi.dev/api/people/1/"
+ *          },
+ *      ],
+ *      "metadata": {
+ *          "totalCount": 3,
+ *          "totalHeightInCM": 2708,
+ *          "totalHeightInFeet": "88.85",
+ *          "totalHeightInInches": "1066.14"
+ *      }
  *     },
  *     "links": []
  * }
